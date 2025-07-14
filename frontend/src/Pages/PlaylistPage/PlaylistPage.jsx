0c4cfd2d519e06bef028f2e20f78394250e3d6fd
jessicaobi <149744440+jessicaobi@users.noreply.github.com>
2025-07-14 13:11:49 -0700

    Adding the Navbar to All of the Relavant pages (#5)
    * Adding the navbar to all of the relavant pages
    
    * Changing the font
    
    ---------
    
    Co-authored-by: Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>

---
+++ frontend/src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../../Components/NavBar/NavBar";
+
+function PlaylistPage() {
+    return (
+        //Displayling NavBar component
+        <NavBar></NavBar>
+     );
+}
+
+export default PlaylistPage;
