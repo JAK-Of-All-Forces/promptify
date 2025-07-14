20f9768a16ace092e890de8c191a13ddc5f0b776
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-14 10:39:43 -0700

    Basic setup of navbar

---
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+
+
+
+function NavBar(){
+return (
+  <nav className="content">
+    {/* Link to homepage on click of the word Promptify*/}
+    <div className="promptify-logo">
+      <Link to = {`/homepage`}>
+        <h1>Promptify</h1>
+      </Link>
+    </div>
+
+    {/* Link to about us page */}
+    <div className="about-us">
+      <Link to={`/about-us`}>
+        <h3>About Us</h3>
+      </Link>
+    </div>
+  </nav>
+);
+}
+
+export default NavBar;
