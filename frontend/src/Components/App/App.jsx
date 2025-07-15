677c8716cb7f02e8e9309114b4af6ac84c47282b
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-15 10:07:11 -0700

    Changes with imports to App.jsx as well as routes

--- frontend/src/Components/App/App.jsx
+++ frontend/src/Components/App/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from '../Pages/HomePage/HomePage';
-import AboutPage from '../Pages/AboutPage/AboutPage';
-import LandingPage from '../Pages/LandingPage/LandingPage';
-import PromptPage from '../Pages/PromptPage/PromptPage';
-import PlaylistPage from '../Pages/PlaylistPage/PlaylistPage';
-import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../../Pages/HomePage/HomePage";
+import AboutPage from "../../Pages/AboutPage/AboutPage";
+import LandingPage from "../../Pages/LandingPage/LandingPage";
+import PromptPage from "../../Pages/PromptPage/PromptPage";
+import PlaylistPage from "../../Pages/PlaylistPage/PlaylistPage";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/prompt" element={<PromptPage />} />
         <Route path="/playlist" element={<PlaylistPage />} />
-        <Route path="*" element={<ErrorPage />} /> 
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
