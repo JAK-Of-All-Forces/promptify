a2d92e905a2b8707ab3a80215e735fdc0d1be85a
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-15 10:23:04 -0700

    Playlist cover working

--- frontend/src/Pages/PlaylistPage/PlaylistPage.jsx
+++ frontend/src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -1,10 +1,27 @@
 import NavBar from "../../Components/NavBar/NavBar";
+import no_image from "../../assets/no_img.png"
+import "./PlaylistPage.css";
 
-function PlaylistPage() {
+function PlaylistPage(playlist) {
     return (
-        //Displayling NavBar component
-        <NavBar></NavBar>
-     );
+        <div>
+            {/* Displayling NavBar component */}
+            <NavBar></NavBar>
+
+            <div>
+
+                <h1 className = "playlist-title">{playlist.title}</h1>
+                <button>Add to Spotify</button>
+                <div className = "playlist-cover">
+                    {playlist.image_url ? (
+                        <img src={playlist.image_url} alt = "Playlist cover" />
+                    ) : (
+                        <img src={no_image} alt="Playlist cover (no image)" />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default PlaylistPage;
