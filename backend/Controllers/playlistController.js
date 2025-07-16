edaca5c3e557a3127b17bb17184a4bd52209b286
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-16 13:32:49 -0700

    Still working on playlist controller, but I am about to pull from main

--- backend/Controllers/playlistController.js
+++ backend/Controllers/playlistController.js
@@ -1,1 +1,13 @@
 const prisma = require("../models/prismaClient");
+
+
+
+exports.getById = async (req, res) => {
+  const id = String(req.params.id);
+
+  const playlist = await prisma.playlist.findUnique({ where: { id } });
+  if (!playlist) {
+    return res.status(404).json({ error: "Playlist not found!" });
+  }
+  res.json(playlist);
+};
--- backend/Controllers/trackController.js
+++ backend/Controllers/trackController.js
@@ -1,1 +1,29 @@
 const prisma = require("../models/prismaClient");
+
+
+//Controller for getting all playlist tracks
+//GET
+exports.getAllPlaylistTracks = async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const allTracks = await prisma.playlist.findUnique({
+      where: { id },
+      include: {
+        cards: true,
+      },
+    });
+
+    if (!allTracks) {
+      return res
+        .status(404)
+        .json({ error: "Tracks for this board cannot be found!" });
+    }
+
+    res.json(allTracks);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong." });
+  }
+};
