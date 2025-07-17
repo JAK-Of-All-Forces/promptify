988eb3c922dba8f33e3ea0b12935de3276f09c78
Amari May <amari.may@amarima-ltmaajm.internal.salesforce.com>
2025-07-17 15:19:25 -0700

    Displaying people's data on About Page

--- frontend/src/Pages/AboutPage/AboutPage.jsx
+++ frontend/src/Pages/AboutPage/AboutPage.jsx
@@ -3,29 +3,45 @@ import blueCard from "../../assets/blueAboutCard.png";
 import purpleCard from "../../assets/purpleAboutCard.png";
 import greenCard from "../../assets/greenAboutCard.png";
 import "../AboutPage/AboutPage.css"; 
+import aboutData from "../../data/aboutData";
+import { useState, useEffect } from "react";
+
+
 
 function AboutPage() {
-    
-    return (
-        <div className="about-container">
-            <NavBar />
-            <div className="aboutCard-container">
-                <img src={blueCard} alt="Blue About Card" className="aboutCard" />
-                <img src={purpleCard} alt="Purple About Card" className="aboutCard" />
-                <img src={greenCard} alt="Green About Card" className="aboutCard" />
-            </div>
-            <div className="faq-container">
-                <h2>Frequently Asked Questions</h2>
-                 <ul>
-                     <li>How does Promptify work?</li>
-                     <li>Is my data secure?</li>
-                     <li>Can I customize my playlists?</li>
-                     <li>Do I need a premium Spotify account?</li>
-                </ul>
-            </div>
+    const [selectedPerson, setSelectedPerson] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false); 
 
-        </div>
-    );
+return (
+  <div className="about-container">
+    <NavBar />
+    <div className="aboutCard-container">
+      {aboutData.map((person) => (
+        <img
+          key={person.id}
+          src={person.image}
+          alt={person.name}
+          className="aboutCard"
+          onClick={() => {
+            setSelectedPerson(person);
+            setModalOpen(true);
+          }}
+        />
+      ))}
+    </div>
+
+    <div className="faq-container">
+      <h2>Frequently Asked Questions</h2>
+      <ul>
+        <li>How does Promptify work?</li>
+        <li>Is my data secure?</li>
+        <li>Can I customize my playlists?</li>
+        <li>Do I need a premium Spotify account?</li>
+      </ul>
+    </div>
+  </div>
+);
 }
+
 
 export default AboutPage;
---
+++ frontend/src/data/aboutData.js
@@ -0,0 +1,25 @@
+const aboutData = [
+  {
+    id: 1,
+    name: "Amari May",
+    role: "heh",
+    image: blueCard,
+    bio: "hehe",
+  },
+  {
+    id: 2,
+    name: "Jessica Obi",
+    role: "yes",
+    image: purpleCard,
+    bio: "Jessi",
+  },
+  {
+    id: 3,
+    name: "Kenna Nyuga-Galega",
+    role: "wow",
+    image: greenCard,
+    bio: "cutie",
+  },
+];
+
+export default aboutData;
