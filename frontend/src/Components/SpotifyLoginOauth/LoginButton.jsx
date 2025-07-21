99b94ff91a3baf7ecb3cd3b82568c67a1cfef88c
Kenna Nyuga-Galega <kennanyuga@gmail.com>
2025-07-21 10:49:22 -0700

    PORT fixes

--- frontend/src/Components/SpotifyLoginOauth/LoginButton.jsx
+++ frontend/src/Components/SpotifyLoginOauth/LoginButton.jsx
@@ -5,22 +5,22 @@ import "./LoginButton.css";
 function LoginButton({}){
     const PORT = import.meta.env.PORT
   
-
-    const PORT = process.env.PORT
-
     const handleLoginClick = async ()=>{
-         try {
-    const { data } = await axios.get(
-      `http://localhost:${PORT}/api/auth/login-url` 
-    );
+    try {
+        const { data } = await axios.get(
+          `http://localhost:${PORT}/api/auth/login-url` 
+        );
 
-    //redirects the user to the spotify login page
-    window.location.href = data.url;
+        //redirects the user to the spotify login page
+        window.location.href = data.url;
 
-  } catch (err) {
-    console.error("Error could not redirect you to the spotify login page : ", err);
-  }
+      } catch (err) {
+        console.error("Error could not redirect you to the spotify login page : ", err);
+      }
     }
+
+
+    
     return(
         <button className="login-button" onClick={handleLoginClick}>
              Login
