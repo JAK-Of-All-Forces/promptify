aad3c0407987d9ec41ce355987ae8be6c60fa327
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-22 11:21:48 -0700

    Changing the backgrounds

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,36 +1,44 @@
 import { Link } from "react-router-dom";
+import {useEffect, useRef} from "react";
+
 import LoginButton from "../../Components/SpotifyLoginOauth/LoginButton";
 import video from "../../assets/search animation.mp4"
 import "./LandingPage.css"
 
 function LandingPage() {
-    function videoSpeed (){
+
+    const videoRef = useRef (null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 2.0;
+            videoRef.current.play();         
+        }
+        }, []);
         
-    }
-    return (
-      <div className="landing-container">
-        <nav>
-          <div className="about-us">
-            <Link to={`/about`}>
-              <h3>About Us</h3>
-            </Link>
-          </div>
-        </nav>
 
-        <div style={{ padding: "2rem" }}>
-          <h1>Welcome to Promptify!</h1>
-          <p>Please log in with Spotify to get started.</p>
+        return (
+        <div className="landing-container">
+            <nav>
+            <div className="about-us">
+                <Link to={`/about`}>
+                <h3>About Us</h3>
+                </Link>
+            </div>
+            </nav>
+            <div style={{ padding: "2rem" }}>
+            <h1>Welcome to Promptify!</h1>
+            <p>Please log in with Spotify to get started.</p>
 
-          <LoginButton />
+            <LoginButton />
+            </div>
+            {/*  */}
+            <video ref = {videoRef} className="video" autoPlay loop muted playsinline>
+            <source src={video}></source>
+            </video>
+            {/* Rest of the landing page content below */}
         </div>
-
-        <video className="video" autoPlay loop muted playsinline>
-          <source src={video}></source>
-        </video>
-
-        {/* Rest of the landing page content below */}
-      </div>
-    );
+        );
 }
 
 export default LandingPage;
