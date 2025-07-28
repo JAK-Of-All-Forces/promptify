0837f56a1c4d3c9acf8a1ac48f0aacf90bbdaac1
Kenna Nyuga-Galega <kennanyuga@gmail.com>
2025-07-28 14:20:53 -0700

    fixing merge fr

--- frontend/src/Components/TrackCard/TrackCard.jsx
+++ frontend/src/Components/TrackCard/TrackCard.jsx
@@ -1,27 +1,36 @@
 //import { Link } from "react-router-dom";
 import no_image from "../../assets/no_img.png";
 import "./TrackCard.css";
-
 function TrackCard({ track }) {
   return (
     <div className="TrackCard">
-        {/* Track cover */}
-        <div className="track-cover">
-          {track.image_url ? (
-            <img src = {track.image_url} alt="Track cover" />
-          ) : (
-            <img src = {no_image} alt="Track cover (no image)" />
-          )}
-        </div>
-
-        {/* Displaying the Track Info */}
-        <div className = "track-info">
-            <p className = "track-name">{track.name}</p>
-            <p className = "track-artist">{track.artist}</p>
-            <p className = "track-duration">{track.duration}</p>
-        </div>
+      {/* Track cover */}
+      <div className="track-cover">
+        {track.image_url ? (
+          <img src={track.image_url} alt="Track cover" />
+        ) : (
+          <img src={no_image} alt="Track cover (no image)" />
+        )}
+      </div>
+      {/* Displaying the Track Info */}
+      <div className="track-info">
+        <p className="track-name">{track.name}</p>
+        <p className="track-artist">{track.artist}</p>
+      </div>
+      <p className="track-duration">{track.duration}</p>
     </div>
   );
 }
-
 export default TrackCard;
+
+
+
+
+
+
+
+
+
+
+
+
