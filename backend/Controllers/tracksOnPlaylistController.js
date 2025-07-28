ffd4ff4ca6353ab20ecf188fafec87a40dc7ae05
Jessica Obi <jessica.obi@jessica-ltmkfac.internal.salesforce.com>
2025-07-28 11:11:05 -0700

    Delete track controller is working

--- backend/Controllers/tracksOnPlaylistController.js
+++ backend/Controllers/tracksOnPlaylistController.js
@@ -12,8 +12,6 @@ exports.getAll = async (req, res) => {
         include: {
           track: true,
         },
-        
-
       }
 
     );
@@ -23,5 +21,28 @@ exports.getAll = async (req, res) => {
   
   catch (error) {
       throw new Error(error);
+  }
+};
+
+
+//DELETE a specific track on a playlist
+exports.deleteTrackById = async (req, res) => {
+  const id = String(req.params.id);
+
+  console.log(id);
+
+  // Get the specific trackOnPlaylist from the database
+  const trackOnPlaylist = await prisma.TracksOnPlaylists.findUnique({ where: { id } });
+
+  if (!trackOnPlaylist) {
+    return res.status(404).json({ error: "This trackOnPlaylist was not found" });
+  }
+
+  try {
+    await prisma.TracksOnPlaylists.delete({ where: { id } });
+  }
+  catch (err) {
+    console.error("Failed to delete track on playlist:", err);
+
   }
 };
--- backend/routes/tracksOnPlaylist.js
+++ backend/routes/tracksOnPlaylist.js
@@ -8,4 +8,8 @@ const controller = require("../Controllers/tracksOnPlaylistController");
 //Get all tracks given the playlist id
 router.get("/:id", controller.getAll);
 
+//Delete tracks given the tracksOnPlaylists id
+router.delete("/:id", controller.deleteTrackById);
+
+
 module.exports = router;
