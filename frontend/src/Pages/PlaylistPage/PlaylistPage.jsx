282539706362a4c2dea7e55c03f7c13c626d21b8
knyuga <kennanyuga@gmail.com>
2025-07-28 18:41:31 -0700

    Media Queries (#46)
    * full width
    
    * slight css change
    
    * css changes and bug fixes
    
    * amapiano
    
    * prompt page media queries for mobile and iphone
    
    * loading page css
    
    * merge?
    
    * typo
    
    * genre addition
    
    * fixing merge fr
    
    * centered
    
    * refelctions reset

--- frontend/src/Pages/PlaylistPage/PlaylistPage.jsx
+++ frontend/src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -4,15 +4,11 @@ import {useEffect, useState} from "react";
 import no_image from "../../assets/no_img.png"
 import "./PlaylistPage.css";
 import TrackCard from "../../Components/TrackCard/TrackCard"
-
 function PlaylistPage({token, setToken}) {
-
     const {id} = useParams();
     const [playlist, setPlaylist] = useState(null);
     console.log("Token (Playlist Page)", token);
     const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-
     //Fetching the playlist using the id from the url
     useEffect(() => {
         const fetchPlaylist = async () => {
@@ -26,20 +22,15 @@ function PlaylistPage({token, setToken}) {
             }
         }
         fetchPlaylist();
-
     }, [id]);
-
     console.log("Playlist", playlist);
-    
     //Add to Spotify function
     async function AddToSpotify(playlist) {
         console.log ("Spotify Playlist", playlist);
-
         //If there is no token upon hitting this page, it will show that the user is not logged in as an alert
         if (!token) {
-            return null; 
+            return null;
         }
-
         try{
             //As the user is logged in up to this point, it will do an API call to the user's playlists on Spotify and do a post request for the playlist
             const addPlaylistRes = await fetch(
@@ -57,7 +48,6 @@ function PlaylistPage({token, setToken}) {
                 }),
                 }
             );
-
             //Error handeling for the creating response
             if (!addPlaylistRes.ok) {
                 const errorData = await addPlaylistRes.json();
@@ -65,21 +55,16 @@ function PlaylistPage({token, setToken}) {
                 `Spotify playlist creation failed: ${errorData.error.message}`
                 );
             }
-
             const createdPlaylist = await addPlaylistRes.json();
-
             //Error handling for the created playlist id
             if (!createdPlaylist.id) {
                 throw new Error("Created playlist ID is missing");
             }
-
-
             //Track URIs are required by the the Spotify API to add tracks to playlists
             //As Promptify fetches the Spotify IDs and not URIs we have to convert it to the correct format when mapping through the playlist's tracks
             const trackURIs = (playlist.tracks || []).map(
                 (track) => `spotify:track:${track.spotifyId}`
             );
-
             //The Spotify API does not accept this call if there are no tracks available so we have to have a conditional for the track length
             if (playlist.tracks.length > 0) {
                 const addTracksRes = await fetch(
@@ -93,7 +78,6 @@ function PlaylistPage({token, setToken}) {
                     body: JSON.stringify({ uris: trackURIs }),
                 }
                 );
-
                 //Error handling for adding tracks
                 if (!addTracksRes.ok) {
                 const errorData = await addTracksRes.json();
@@ -102,33 +86,21 @@ function PlaylistPage({token, setToken}) {
                 );
                 }
             }
-
             alert("Playlist added to Spotify!");
-
             } catch (err) {
             console.error("Error adding to Spotify:", err);
             alert("Failed to add playlist to Spotify.");
         }
         }
-
-
-
-
-
-
-
-
     if (!playlist) {
         return <div className="PlaylistPage">Loading...</div>;
     }
-
     return (
       <div className="playlist-page">
         {/* Displayling NavBar component */}
       <NavBar token={token}></NavBar>
         {/* Playlist Page */}
           {/* Add to Spotify Button */}
-
           <div className="playlist-content">
             {/* Playlist Cover */}
             <div className = "left-container">
@@ -139,7 +111,6 @@ function PlaylistPage({token, setToken}) {
                   <img src={no_image} alt="Playlist cover (no image)" />
                 )}
               </div>
-
               {/* Playlist Title */}
               <h1 className="playlist-title">{playlist.name}</h1>
               <div className="button-container">
@@ -151,7 +122,6 @@ function PlaylistPage({token, setToken}) {
                 </button>
               </div>
             </div>
-
             {/* Mapping through all of the playlist tracks */}
             <div className="playlist-tracks">
               {!playlist.tracks || playlist.tracks.length === 0 ? (
@@ -169,5 +139,4 @@ function PlaylistPage({token, setToken}) {
         </div>
     );
 }
-
 export default PlaylistPage;
